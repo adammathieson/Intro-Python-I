db1bccdb03a72c6f625b4cd221fd615cc642a266
Adam Mathieson <mathieson1222@gmail.com>
2019-10-01 15:31:39 -0700

    tricky figuring out the ** for kwargs, and spacing/format was an issue

--- src/11_args.py
+++ src/11_args.py
@@ -7,13 +7,20 @@
 # YOUR CODE HERE
 def f1(a, b):
     return a + b
-    
-print(f1(1, 2))
+
+print("f1:", f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+def f2(*num):
+    sum = 0
+    for n in num:
+        sum = sum + n
+    return sum
+
+
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -23,14 +30,15 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
-
+def f3(a, b = int(1)):
+    return a + b
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
 
@@ -44,6 +52,9 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**data):
+    for k, v in data.items():
+        print("key: {}, value: {}".format(k, v))
 
 # Should print
 # key: a, value: 12
@@ -62,4 +73,4 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-f4(d)
+f4(**d)
